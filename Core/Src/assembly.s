848053252e86a813204a5c496f27d58a3bf1f413
mtsvuy022 <146190337+vuyani02@users.noreply.github.com>
2024-10-12 15:01:27 +0200

    reimplemented the way leds increment

--- Core/Src/assembly.s
+++ Core/Src/assembly.s
@@ -1,9 +1,9 @@
 /*
-* assembly.s
-*
-*/
-
-@ DO NOT EDIT
+ * assembly.s
+ *
+ */
+ 
+ @ DO NOT EDIT
 	.syntax unified
     .text
     .global ASM_Main
@@ -36,9 +36,30 @@ ASM_Main:
 
 main_loop:
 
+    @ If SW0 is not pressed, set short delay
+	LDR R3, LONG_DELAY_CNT
+	MOVS R4, 0
+
+
+
     LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
 	MOVS R1, #0b11111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
 	STR R1, [R0, #40]
+
+
+
+	@ Check if SW1 (PA1) is pressed
+	LDR R0, GPIOA_BASE      @ Load GPIOA base address into R0
+	LDR R1, [R0, #0x10]     @ Read GPIOA_IDR (input data register)
+	LSLS R1, R1, #30        @ Shift left by 30 to move bit 1 (PA1) to the MSB
+	LSRS R1, R1, #31        @ Shift right by 31 to move MSB (original bit 1) to the LSB
+	CMP R1, #0              @ Compare R1 with 0 to check if SW1 is pressed
+	BEQ set_short_delay
+
+
+    B continue  @ If SW1 is not pressed, skip the BL
+
+continue:
 
 	@ Check if SW0 (PA0) is pressed
 	LDR R0, GPIOA_BASE      @ Load GPIOA base address into R0
@@ -46,165 +67,55 @@ main_loop:
 	LSLS R1, R1, #31        @ Test if bit 0 (PA0) is low (SW0 pressed)
 	BEQ increament_by_2     @ If pressed, branch to set_long_delay
 
-	@ Check if SW1 (PA1) is pressed
-	LDR R0, GPIOA_BASE      @ Load GPIOA base address into R0
-	LDR R1, [R0, #0x10]     @ Read GPIOA_IDR (input data register)
-	LSLS R1, R1, #30        @ Shift left by 30 to move bit 1 (PA1) to the MSB
-	LSRS R1, R1, #31        @ Shift right by 31 to move MSB (original bit 1) to the LSB
-	BEQ set_shot_delay     @ If SW1 is pressed (PA1 == 0), branch to increment_by_2
 
 	@ Check if SW2 (PA2) is pressed
 	LDR R0, GPIOA_BASE      @ Load GPIOA base address into R0
 	LDR R1, [R0, #0x10]     @ Read GPIOA_IDR (input data register)
 	LSLS R1, R1, #29        @ Shift left by 29 to move bit 2 (PA2) to the MSB
 	LSRS R1, R1, #31        @ Shift right by 31 to move MSB (original bit 2) to the LSB
-	BEQ sw2_pattern     @ If SW2 is pressed (PA2 == 0), branch to increment_by_2
-
-	@ If SW0 is not pressed, set short delay
-	LDR R3, LONG_DELAY_CNT
-	B set_delay
-
-set_shot_delay:
-	@ Set long delay (5 seconds)
-	LDR R3, SHORT_DELAY_CNT
+	BEQ sw2_pattern         @ If SW2 is pressed (PA2 == 0), branch to increment_by_2
 
 set_delay:
 	@ Turn LEDs on
 	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00000001    
+	MOV R1, R4
 	STR R1, [R0, #24]
 	MOV R2, R3
 	BL delay
+	BL freeze
 
-	@ Check if SW3 (PA3) is pressed
-	LDR R0, GPIOA_BASE      @ Load GPIOA base address into R0
-	LDR R1, [R0, #0x10]     @ Read GPIOA_IDR (input data register)
-	LSLS R1, R1, #28        @ Shift left by 28 to move bit 3 (PA3) to the MSB
-	LSRS R1, R1, #31        @ Shift right by 31 to move MSB (original bit 3) to the LSB
-	CMP R1, #0              @ Compare bit 3 to 0 (pressed)
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00000011    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00000111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00001111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00011111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b01111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b11111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
+	MOV R5, R4
+	MOVS R7, 255
+	SUBS R6, R5, R7
+	BEQ main_loop
+	ADDS R4, #1
 	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
 	MOVS R1, #0b11111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
 	STR R1, [R0, #40]
-	MOV R2, R3
-	BL delay
+	B set_delay
 
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	B main_loop          	@ Infinite loop
+set_short_delay:          @ Subroutine to set short delay
+    LDR R3, SHORT_DELAY_CNT  @ Set short delay (e.g., 1 second)
+    B continue                   @ Return to the point where BL was called
 
 increament_by_2:
-
 	@ Turn LEDs on
 	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00000011
+	MOV R1, R4
 	STR R1, [R0, #24]
 	MOV R2, R3
 	BL delay
+	BL freeze
 
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00001111    @ Reset all LEDs (GPIOB Pins 0-7) to of
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b00111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay              	@ Call delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	@ Turn LEDs off
-	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
-	MOVS R1, #0b11111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
-	STR R1, [R0, #24]
-	MOV R2, R3
-	BL delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
+	MOV R5, R4
+	MOVS R7, 254
+	SUBS R6, R5, R7
+	BEQ main_loop
+	ADDS R4, #2
 	LDR R0, GPIOB_BASE   	@ Load GPIOB base address into R0
 	MOVS R1, #0b11111111    @ Reset all LEDs (GPIOB Pins 0-7) to off
 	STR R1, [R0, #40]
-	MOV R2, R3
-	BL delay
-
-	BL freeze               @ If SW3 is pressed (PA3 == 0), call increament_by_2 subroutine
-
-	B main_loop          	@ Infinite loop
+	B increament_by_2
 
 sw2_pattern:
 
@@ -218,7 +129,7 @@ sw2_pattern:
 	B main_loop          	@ Infinite loop
 
 freeze:
-    @ Check if SW3 (PA3) is pressed
+   @ Check if SW3 (PA3) is pressed
 	LDR R0, GPIOA_BASE      @ Load GPIOA base address into R0
 	LDR R1, [R0, #0x10]     @ Read GPIOA_IDR (input data register)
 	LSLS R1, R1, #28        @ Shift left by 28 to move bit 3 (PA3) to the MSB
@@ -232,10 +143,6 @@ delay_loop:
 	BNE delay_loop          @ If not zero, keep looping
 	BX LR                   @ Return from delay
 
-write_leds:
-	STR R2, [R1, #0x14]
-	B main_loop
-
 @ LITERALS; DO NOT EDIT
 	.align
 RCC_BASE: 			.word 0x40021000
@@ -245,5 +152,5 @@ GPIOB_BASE:  		.word 0x48000400
 MODER_OUTPUT: 		.word 0x5555
 
 @ TODO: Add your own values for these delays
-LONG_DELAY_CNT: 	.word 1400000
+LONG_DELAY_CNT: 	.word 100000
 SHORT_DELAY_CNT: 	.word 600000
